6e39631f8be5b574fb45177b44abf842136d7034
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-04-05 09:15:23 +0900

    first commit

---
+++ Module/Lottomodule.js
@@ -0,0 +1,27 @@
+function Lotto(Kakao, roomName, sender) {
+    try {
+        let lotto = [];
+        while (lotto.length < 6) {
+            let number = parseInt(Math.random() * 45) + 1;
+            if (lotto.indexOf(number) < 0) {
+                lotto.push(number);
+            }
+        }
+        lotto.sort(function (a, b) {
+            return a - b;
+        });
+        Kakao.sendLink(roomName, {
+            template_id: 72728,
+            template_args:
+            {
+                "person": sender,
+                "lottonumber": lotto.toString()
+            }
+        }, "custom");
+    }
+    catch (error) {
+        replier.reply(error);
+    }
+}
+
+module.exports = Lotto;
